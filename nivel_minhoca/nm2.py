7e609bfe1db42a836b54f66195e4d0e7dcf03f19
Rafiki <rafiki.silva@al.infnet.edu.br>
2024-09-08 20:17:59 -0300

    feat(substituir_palavra)

---
+++ nivel_minhoca/nm2.py
@@ -0,0 +1,10 @@
+# Crie um script em Python que substitua todas as ocorrências de uma palavra específica em uma frase por outra palavra fornecida pelo usuário. Utilize um texto de exemplo de sua preferência e escolha a palavra a ser substituída, mas a lógica precisa funcionar para outros casos.
+
+def substituir_palavra(frase, palavra_antiga, palavra_nova):
+    frase = frase.replace(palavra_antiga, palavra_nova)
+    return frase
+
+frase = "Não deixe o samba morrer, não deixe o samba acabar, o morro foi feito de samba, de samba pra gente sambaar"
+palavra_antiga = "samba"
+palavra_nova = input("Digite a palavra que deseja substituir: ")
+print(substituir_palavra(frase, palavra_antiga, palavra_nova))
