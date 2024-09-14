1bc9537ce1032082426d15df7dbef3fa5fedbab1
Rafiki <rafiki.silva@al.infnet.edu.br>
2024-09-13 21:41:26 -0300

    docs: adição de docstring

--- nivel_minhoca/nm5.py
+++ nivel_minhoca/nm5.py
@@ -1,6 +1,14 @@
-# Calcular a soma dos dígitos em uma string numérica fornecida pelo usuário, verificando se todos os caracteres são de fato numéricos.
-
 def somar_digitos(string):
+    """
+    Soma todos os dígitos numéricos presentes em uma string.
+
+    Args:
+        string (str): A string que pode conter dígitos e outros caracteres.
+
+    Returns:
+        int: A soma de todos os dígitos encontrados na string.
+             Se a string não contiver dígitos, retorna 0.
+    """
     soma = 0
     for caracter in string:
         if caracter.isdigit():
