837e1ee26d392f3d760cfcb4ff485e06ebc5f1b6
Rafiki <rafiki.silva@al.infnet.edu.br>
2024-09-13 21:49:31 -0300

    docs: adição de docstring

--- nivel_cobrinha/nc1.py
+++ nivel_cobrinha/nc1.py
@@ -1,6 +1,13 @@
-# Calcular o resultado de uma expressão matemática básica fornecida como string pelo usuário, ignorando espaços, permitindo apenas caracteres numéricos e os operadores +, -, * e /.
-
 def calcular_expressao(expressao):
+    """
+    Avalia uma expressão matemática básica composta por números e operadores aritméticos (+, -, *, /).
+
+    Args:
+        expressao (str): A expressão matemática a ser avaliada. Deve conter apenas números e os operadores aritméticos.
+
+    Returns:
+        float or str: O resultado da expressão se for válida. Caso contrário, retorna 'Expressão inválida.'.
+    """
     for caracter in expressao:
         if caracter not in "1234567890+-*/":
             return "Expressão inválida."
